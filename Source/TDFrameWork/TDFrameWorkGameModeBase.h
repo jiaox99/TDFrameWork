28b5b003a7ecf8486f918437e276304c314344a2
jiaox99 <jiaox99@163.com>
2019-04-04 10:26:09 +0800

    Initial commit

---
+++ Source/TDFrameWork/TDFrameWorkGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TDFrameWorkGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TDFRAMEWORK_API ATDFrameWorkGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
